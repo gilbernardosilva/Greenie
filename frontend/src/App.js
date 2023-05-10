d424de53913677ec8715705ecdf44dd7bd1b3740
bitorhugo <vitorhugosantos144@gmail.com>
2023-05-10 22:19:04 +0100

    testing connectivity between fron and back end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,59 @@
-import React, { useState } from "react";
-import Chatbot from "react-chatbot-kit";
-import FloatingButton from "./FloatingButton";
-import config from "./chatbot/config";
-import ActionProvider from "./chatbot/ActionProvider";
-import MessageParser from "./chatbot/MessageParser";
+import React, { Component } from 'react';
 
-
-function App() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleToggleChatbot = () => {
-    setIsOpen(!isOpen);
+class App extends Component {
+  state = {
+    day: null
   };
-
-  return (
-    <div className="App">
-      <FloatingButton isOpen={isOpen} handleClick={handleToggleChatbot} />
-      {isOpen && (
-        <div className="chatbot-container">
-          <Chatbot
-            config={config}
-            actionProvider={ActionProvider}
-            messageParser={MessageParser}
-          />
-        </div>
-      )}
-    </div>
-  );
     
+    componentDidMount() {
+    fetch('/api/day')
+      .then(response => response.json())
+      .then(data => this.setState({ day: data }));
+    }
+    
+  render() {
+    const { day } = this.state;
+    return (
+      <div>
+          <p>Today is {day}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
+// import React, { useState } from "react";
+// import Chatbot from "react-chatbot-kit";
+// import FloatingButton from "./FloatingButton";
+// import config from "./chatbot/config";
+// import ActionProvider from "./chatbot/ActionProvider";
+// import MessageParser from "./chatbot/MessageParser";
+
+
+// function App() {
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   const handleToggleChatbot = () => {
+//     setIsOpen(!isOpen);
+//   };
+
+//   return (
+//     <div className="App">
+//       <FloatingButton isOpen={isOpen} handleClick={handleToggleChatbot} />
+//       {isOpen && (
+//         <div className="chatbot-container">
+//           <Chatbot
+//             config={config}
+//             actionProvider={ActionProvider}
+//             messageParser={MessageParser}
+//           />
+//         </div>
+//       )}
+//     </div>
+//   );
+    
+// }
+
+// export default App;
