3e1e8b2bab4fb188f48a87de142074f4255b34bf
bitorhugo <vitorhugosantos144@gmail.com>
2023-05-15 22:09:02 +0100

    ADD: upload file route

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from pathlib import Path
 from datetime import datetime
-from app.greenie import chat
+from greenie import chat
 
 app = FastAPI()
 
@@ -17,3 +18,19 @@ async def get_asnwer(question: str):
     Get an answer for a question
     """
     return chat(question)
+
+@app.post("/upload-file/")
+async def upload_file(file: UploadFile = File(...)):
+    # Create a directory to save the uploaded files
+    path = 'uploads/'
+    upload_dir = Path(path)
+    upload_dir.mkdir(parents=True, exist_ok=True)
+
+    # Save the file to the upload directory
+    file_path = path + str(file.filename)
+    content = await file.read()
+    with open(file_path, "wb") as f:
+        f.write(content)
+
+    return {"filename": file.filename}
+
